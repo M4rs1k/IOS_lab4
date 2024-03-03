62f8d7412d31942f0d9b59e877870984552fdaa0
M4rs1k <130493365+M4rs1k@users.noreply.github.com>
2024-03-04 00:24:01 +0600

    Add files via upload

---
+++ TO DO - Lab 4 /toDo/EditToDoViewController.swift
@@ -0,0 +1,89 @@
+import UIKit
+import CoreData
+
+class EditToDoViewController: UIViewController, UITextFieldDelegate, UITextViewDelegate {
+    
+    let context = AppDelegate.viewContext
+    
+    var toDoItem: ToDoItem?
+    
+    @IBOutlet weak var titleTextField: UITextField! {
+        didSet {
+            titleTextField.delegate = self
+        }
+    }
+    
+    @IBOutlet weak var descTextView: UITextView! {
+        didSet {
+            descTextView.delegate = self
+        }
+    }
+    
+    let defaultTitle = "New todo"
+    let defaultDesc = "Describe it here..."
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor(patternImage: UIImage(named: "paper.jpg")!)
+        titleTextField.text = toDoItem?.title ?? defaultTitle
+        descTextView.text = toDoItem?.desc ?? defaultDesc
+        descTextView.textColor = UIColor.gray
+        saveData()
+    }
+    
+    func saveData() {
+        let date = Date()
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd/MM/YY"
+        let currentDate = dateFormatter.string(from: date)
+        toDoItem?.date = currentDate
+        
+        let timeFormatter = DateFormatter()
+        timeFormatter.timeStyle = .short
+        let currentTime = timeFormatter.string(from: date)
+        toDoItem?.time = currentTime
+        
+        toDoItem?.title = titleTextField.text != "" ? titleTextField.text : defaultTitle
+        toDoItem?.desc = descTextView.text != "" ? descTextView.text : defaultDesc
+        
+        try? context.save()
+    }
+    
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        saveData()
+        return true
+    }
+    
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        if textField.text == defaultTitle {
+            textField.text = ""
+        }
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        saveData()
+        return true
+    }
+    
+    func textViewShouldBeginEditing(_ textView: UITextView) -> Bool {
+        if textView.text == defaultDesc {
+            textView.text = ""
+        }
+        if textView.textColor == UIColor.gray {
+            textView.textColor = UIColor.black
+        }
+        return true
+    }
+    
+    func textViewShouldEndEditing(_ textView: UITextView) -> Bool {
+        textView.resignFirstResponder()
+        textView.textColor = UIColor.gray
+        saveData()
+        return true
+    }
+}
---
+++ TO DO - Lab 4 /toDo/ToDoTableViewController.swift
@@ -0,0 +1,117 @@
+import UIKit
+import CoreData
+
+class ToDoTableViewController: UITableViewController {
+    
+    let context = AppDelegate.viewContext
+    
+    var toDosPending = [ToDoItem]()
+    var toDosCompleted = [ToDoItem]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.view.backgroundColor = UIColor(patternImage: UIImage(named: "paper.jpg")!)
+        tableView.backgroundColor = UIColor.clear
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        fetchToDos()
+        tableView.reloadData()
+    }
+    
+    func fetchToDos() {
+        let request: NSFetchRequest<ToDoItem> = ToDoItem.fetchRequest()
+        if let toDoItems = try? context.fetch(request) {
+            toDosPending = []
+            toDosCompleted = []
+            toDoItems.reversed().forEach { toDoItem in
+                if !toDoItem.completed { toDosPending.append(toDoItem) } else { toDosCompleted.append(toDoItem) }
+            }
+        }
+    }
+    
+    @IBAction func addToDo(_ sender: UIBarButtonItem) {
+        let toDoItem = ToDoItem(context: context)
+        try? context.save()
+        performSegue(withIdentifier: "editToDo", sender: toDoItem)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "editToDo" {
+            if let toDoItem = sender as? ToDoItem {
+                if let editToDoVC = segue.destination as? EditToDoViewController {
+                    editToDoVC.toDoItem = toDoItem
+                }
+            }
+        }
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section == 0, !toDosPending.isEmpty {
+            return "Pending"
+        }
+        if section == 1, !toDosCompleted.isEmpty {
+            return "Completed"
+        }
+        return nil
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return section == 0 ? toDosPending.count : toDosCompleted.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "toDoCell", for: indexPath)
+        
+        let toDo = indexPath.section == 0 ? toDosPending[indexPath.row] : toDosCompleted[indexPath.row]
+        
+        cell.textLabel?.text = toDo.title
+        if let date = toDo.date, let time = toDo.time {
+            let timeStamp = "\(date) \(time)"
+            cell.detailTextLabel?.text = timeStamp
+            cell.detailTextLabel?.textColor = UIColor.lightGray
+        }
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        if indexPath.section == 1 {
+            return nil
+        }
+        return indexPath
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.section == 0 {
+            let toDoItem = toDosPending[indexPath.row]
+            performSegue(withIdentifier: "editToDo", sender: toDoItem)
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let toDoItem = indexPath.section == 0 ? toDosPending[indexPath.row] : toDosCompleted[indexPath.row]
+        let completedAction = UIContextualAction(style: .normal, title: indexPath.section == 0 ? "Done" : "Not Done")
+        { (action, view, handler) in
+            toDoItem.completed = !toDoItem.completed
+            try? self.context.save()
+            self.fetchToDos()
+            tableView.reloadData()
+        }
+        completedAction.backgroundColor = UIColor.green
+        return UISwipeActionsConfiguration(actions: [completedAction])
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        let toDoItem = indexPath.section == 0 ? toDosPending[indexPath.row] : toDosCompleted[indexPath.row]
+        if editingStyle == .delete {
+            context.delete(toDoItem)
+            try? context.save()
+            fetchToDos()
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+}
